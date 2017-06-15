1349a4399fa25183827e786bacdc34fef10dc54f
ddimitrioglo <ddimitrioglo@pycoding.biz>
2017-06-15 14:21:11 +0300

    #ASH add email template to export

--- bootstrap.js
+++ bootstrap.js
@@ -1,7 +1,9 @@
 const EmailSender = require('./src/EmailSender');
+const EmailTemplate = require('./src/EmailTemplate');
 const CaptchaChecker = require('./src/CaptchaChecker');
 
 module.exports = {
   EmailSender: EmailSender,
+  EmailTemplate: EmailTemplate,
   CaptchaChecker: CaptchaChecker,
 };
